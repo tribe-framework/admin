7c5b86bce186f17c7e2d375a72dd37f40b178d95
akshay / wildfire <akshay.wildfire@pm.me>
2020-12-01 12:48:58 +0530

    get_uploader_path

--- uploader.php
+++ uploader.php
@@ -4,7 +4,9 @@ include_once ('../../../tribe.init.php');
 //https://github.com/blueimp/jQuery-File-Upload/blob/master/server/php/UploadHandler.php
 include_once(__DIR__.'/plugins/blueimp-jquery-file-upload/UploadHandler.php');
 
+$upload_paths=$dash->get_uploader_path();
+
 if (defined('UPLOAD_FILE_TYPES'))
-	$upload_handler = new UploadHandler(array('script_url'=>__DIR__.'/uploader.php', 'upload_dir'=>ABSOLUTE_PATH.'/uploads/'.date('Y').'/'.date('m-F').'/'.date('d-D').'/', 'upload_url'=>BASE_URL.'/uploads/'.date('Y').'/'.date('m-F').'/'.date('d-D').'/', 'inline_file_types'=>UPLOAD_FILE_TYPES, 'accept_file_types'=>UPLOAD_FILE_TYPES));
+	$upload_handler = new UploadHandler(array('script_url'=>__DIR__.'/uploader.php', 'upload_dir'=>$upload_paths['upload_dir'], 'upload_url'=>$upload_paths['upload_url'], 'inline_file_types'=>UPLOAD_FILE_TYPES, 'accept_file_types'=>UPLOAD_FILE_TYPES));
 else
-	$upload_handler = new UploadHandler(array('script_url'=>__DIR__.'/uploader.php', 'upload_dir'=>ABSOLUTE_PATH.'/uploads/'.date('Y').'/'.date('m-F').'/'.date('d-D').'/', 'upload_url'=>BASE_URL.'/uploads/'.date('Y').'/'.date('m-F').'/'.date('d-D').'/'));
+	$upload_handler = new UploadHandler(array('script_url'=>__DIR__.'/uploader.php', 'upload_dir'=>$upload_paths['upload_dir'], 'upload_url'=>$upload_paths['upload_url']));
