f7a76d1513d5348cabd6e2ed0fa4a1b12859478a
akshay / wildfire <akshay.wildfire@pm.me>
2020-12-01 12:37:23 +0530

    removed upload height and width restriction

--- uploader.php
+++ uploader.php
@@ -5,6 +5,6 @@ include_once ('../../../tribe.init.php');
 include_once(__DIR__.'/plugins/blueimp-jquery-file-upload/UploadHandler.php');
 
 if (defined('UPLOAD_FILE_TYPES'))
-	$upload_handler = new UploadHandler(array('script_url'=>__DIR__.'/uploader.php', 'upload_dir'=>ABSOLUTE_PATH.'/uploads/'.date('Y').'/'.date('m-F').'/'.date('d-D').'/', 'upload_url'=>BASE_URL.'/uploads/'.date('Y').'/'.date('m-F').'/'.date('d-D').'/', 'inline_file_types'=>UPLOAD_FILE_TYPES, 'accept_file_types'=>UPLOAD_FILE_TYPES, 'max_height'=>(defined(UPLOAD_MAX_HEIGHT)?UPLOAD_MAX_HEIGHT:1440), 'max_width'=>(defined(UPLOAD_MAX_WIDTH)?UPLOAD_MAX_WIDTH:1440)));
+	$upload_handler = new UploadHandler(array('script_url'=>__DIR__.'/uploader.php', 'upload_dir'=>ABSOLUTE_PATH.'/uploads/'.date('Y').'/'.date('m-F').'/'.date('d-D').'/', 'upload_url'=>BASE_URL.'/uploads/'.date('Y').'/'.date('m-F').'/'.date('d-D').'/', 'inline_file_types'=>UPLOAD_FILE_TYPES, 'accept_file_types'=>UPLOAD_FILE_TYPES));
 else
-	$upload_handler = new UploadHandler(array('script_url'=>__DIR__.'/uploader.php', 'upload_dir'=>ABSOLUTE_PATH.'/uploads/'.date('Y').'/'.date('m-F').'/'.date('d-D').'/', 'upload_url'=>BASE_URL.'/uploads/'.date('Y').'/'.date('m-F').'/'.date('d-D').'/', 'max_height'=>(defined(UPLOAD_MAX_HEIGHT)?UPLOAD_MAX_HEIGHT:1440), 'max_width'=>(defined(UPLOAD_MAX_WIDTH)?UPLOAD_MAX_WIDTH:1440)));
+	$upload_handler = new UploadHandler(array('script_url'=>__DIR__.'/uploader.php', 'upload_dir'=>ABSOLUTE_PATH.'/uploads/'.date('Y').'/'.date('m-F').'/'.date('d-D').'/', 'upload_url'=>BASE_URL.'/uploads/'.date('Y').'/'.date('m-F').'/'.date('d-D').'/'));
