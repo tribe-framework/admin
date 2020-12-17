a06ee4b4c980595441dcc7a74c19ccbeee163443
akshay / wildfire <akshay.wildfire@pm.me>
2020-12-17 15:59:47 +0530

    uploader

--- uploader.php
+++ uploader.php
@@ -7,7 +7,7 @@ include_once __DIR__ . '/plugins/blueimp-jquery-file-upload/UploadHandler.php';
 $upload_paths = $dash->get_uploader_path();
 
 if (defined('UPLOAD_FILE_TYPES')) {
-	$upload_handler = new UploadHandler(array('script_url' => __DIR__ . '/uploader.php', 'upload_dir' => $upload_paths['upload_dir'] . '/', 'upload_url' => $upload_paths['upload_url'] . '/', 'inline_file_types' => UPLOAD_FILE_TYPES, 'accept_file_types' => UPLOAD_FILE_TYPES));
+	$upload_handler = new UploadHandler(array('script_url' => '/admin/uploader', 'upload_dir' => $upload_paths['upload_dir'] . '/', 'upload_url' => $upload_paths['upload_url'] . '/', 'inline_file_types' => UPLOAD_FILE_TYPES, 'accept_file_types' => UPLOAD_FILE_TYPES));
 } else {
-	$upload_handler = new UploadHandler(array('script_url' => __DIR__ . '/uploader.php', 'upload_dir' => $upload_paths['upload_dir'] . '/', 'upload_url' => $upload_paths['upload_url'] . '/'));
+	$upload_handler = new UploadHandler(array('script_url' => '/admin/uploader', 'upload_dir' => $upload_paths['upload_dir'] . '/', 'upload_url' => $upload_paths['upload_url'] . '/'));
 }
