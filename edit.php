30f4e0b49658801633ba2a372d7caba803c44734
Apurv Jyotirmay <apjyotirmay@gmail.com>
2020-12-19 14:59:00 +0530

    fixed layout of admin edit form's uploaded files

--- edit.php
+++ edit.php
@@ -1,6 +1,10 @@
 <?php
-include_once __DIR__ . '/header.php';
+use Wildfire\Core\Dash;
 
+include_once __DIR__.'/header.php';
+
+$dash = new Dash();
+$types = $dash->getTypes();
 $role = null;
 
 if (isset($_GET['id'])) {
@@ -26,102 +30,136 @@ else:
             $pid = $dash->get_next_id();
         }
 
-        ?>
+?>
 
-										        <link rel="stylesheet" type="text/css" href="<?=ADMIN_URL;?>/plugins/typeout/typeout.css">
+<link rel="stylesheet" type="text/css" href="<?=ADMIN_URL?>/plugins/typeout/typeout.css">
 
-										        <div class="container">
-										            <a name="infos"></a>
-										            <div id="infos" class="d-none alert alert-success"></div>
+<div class="container">
+    <a name="infos"></a>
+    <div id="infos" class="d-none alert alert-success"></div>
 
-										            <a name="errors"></a>
-										            <div id="errors" class="d-none alert alert-danger"></div>
-										        </div>
+    <a name="errors"></a>
+    <div id="errors" class="d-none alert alert-danger"></div>
+</div>
 
 
-										        <form method="post" class="edit_form" action="/admin/json" autocomplete="off">
-										            <?=
+<form method="post" class="edit_form" action="/admin/json" autocomplete="off">
+    <?=
         $admin->get_admin_menu(
             $types[$type]['disallow_editing'] ?
-            'view' :
-            'edit',
+				'view' :
+				'edit',
             $type,
             $role['slug'] ?? '',
             $_GET['id'] ?? ''
         );
-        ?>
+	?>
 
-										            <h2 class="form_title"><?php echo($type == 'user' ? $role['title'] . '&nbsp;<small><span class="fas fa-angle-double-right"></span></small>&nbsp;' : '') . 'Edit ' . $types[$type]['name']; ?></h2>
+    <h2 class="form_title">
+	<?php if ($type === 'user'): ?>
+		<?=$role['title']?>&nbsp;<small><span class="fas fa-angle-double-right"></span></small>&nbsp;
+	<?php endif; ?>
+		Edit <?=$types[$type]['name']?>
+    </h2>
 
-										            <div class="form-style">
-										                <?php include 'form.php';?>
-										            </div>
+    <div class="form-style">
+		<?php include __DIR__.'/form.php';?>
+    </div>
 
-										            <input type="hidden" name="class" value="dash">
-										            <?php
-        if ($role['slug']) {
-            echo '<input type="hidden" name="role_slug" value="' . $role['slug'] . '">';
-        } elseif ($post['role_slug']) {
-            echo '<input type="hidden" name="role_slug" value="' . $post['role_slug'] . '">';
-        }
+    <input type="hidden" name="class" value="dash">
 
-    if (($types['webapp']['allow_type_change'] ?? false) && ($types[$type]['type'] == 'content')) {
-        echo '
-					                <div class="form-group mt-5"><select class="form-control pl-0 border-top-0 border-left-0 border-right-0 rounded-0 mt-1" id="select_type" name="type">';
-        if (!($post_type = $post['type'])) {
-            $post_type = $_GET['type'];
-        }
+	<?php if ($role['slug']):?>
+		<input type="hidden" name="role_slug" value="<?=$role['slug']?>">
+	<?php elseif ($post['role_slug']): ?>
+		<input type="hidden" name="role_slug" value="<?=$post['role_slug']?>">
+	<?php endif ?>
 
-        foreach ($types as $key => $value) {
-            if ($types[$key]['type'] == 'content') {
-                echo '<option value="' . $types[$key]['slug'] . '" ' . (($types[$key]['slug'] == $post_type) ? 'selected="selected"' : '') . '>' . ucfirst($types[$key]['name']) . '</option>';
-            }
-        }
-        echo '</select><div class="col-12 row text-muted small m-0"><span class="ml-auto mr-0">Change content type (rarely used, use with caution)</div></div>';
-    } else {
-        echo '<input type="hidden" name="type" value="' . $types[$type]['slug'] . '">';
-    }
+	<?php
+	if (
+		($types['webapp']['allow_type_change'] ?? false) &&
+		($types[$type]['type'] == 'content')
+	):
+	?>
+		<div class="form-group mt-5">
+			<select
+				class="form-control pl-0 border-top-0 border-left-0 border-right-0 rounded-0 mt-1"
+				id="select_type"
+				name="type"
+			>
+			<?php
+				if (!($post_type = $post['type'])) {
+					$post_type = $_GET['type'];
+				}
+			?>
+			<?php foreach ($types as $key => $value): ?>
+				<?php if ($types[$key]['type'] == 'content'): ?>
+					<option
+						value="<?=$types[$key]['slug']?>"
+						<?=$types[$key]['slug'] === $post_type ? 'selected="selected"' : ''?>
+					>
+						<?=ucfirst($types[$key]['name'])?>
+					</option>
+				<?php endif ?>
+			<?php endforeach ?>
+			</select>
 
-    ?>
-					            <?php echo($types[$type]['type'] == 'content' ? '<input type="hidden" name="user_id" value="' . ($post['user_id'] ? $post['user_id'] : $session_user['user_id']) . '">' : ''); ?>
-					            <input type="hidden" name="function" value="push_content">
-					            <input type="hidden" name="id" value="<?php echo $_GET['id']; ?>">
-					            <input type="hidden" name="slug" value="<?php echo $post['slug']; ?>">
+			<div class="col-12 row text-muted small m-0">
+				<span class="ml-auto mr-0">Change content type (rarely used, use with caution)</span>
+			</div>
+		</div>
+	<?php else: ?>
+		<input type="hidden" name="type" value="<?=$types[$type]['slug']?>">
+	<?php endif ?>
 
-					            <?php if (count($types[$type]['modules']) > 3) {
-        echo $admin->get_admin_menu(($types[$type]['disallow_editing'] ? 'view' : 'edit'), $type, $role['slug'], $_GET['id']);
+	<?php if ($types[$type]['type'] == 'content'): ?>
+		<input
+			type="hidden"
+			name="user_id"
+			value="<?=$post['user_id'] ?: $session_user['user_id']?>"
+		>
+	<?php endif ?>
+
+    <input type="hidden" name="function" value="push_content">
+    <input type="hidden" name="id" value="<?=$_GET['id']?>">
+    <input type="hidden" name="slug" value="<?=$post['slug']?>">
+
+    <?php if (count($types[$type]['modules']) > 3) {
+        echo $admin->get_admin_menu(
+			($types[$type]['disallow_editing'] ? 'view' : 'edit'),
+			$type,
+			$role['slug'],
+			$_GET['id']
+		);
     }?>
-					            <p>&nbsp;</p>
-					        </form>
+    <p>&nbsp;</p>
+</form>
 
-					        <div class="modal fade" id="delete_conf_<?php echo $_GET['id']; ?>" tabindex="-1" role="dialog">
-					        <div class="modal-dialog" role="document">
-					            <div class="modal-content">
-					            <div class="modal-header">
-					                <h5 class="modal-title">Confirm</h5>
-					                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-					                <span aria-hidden="true">&times;</span>
-					                </button>
-					            </div>
-					            <div class="modal-body">
-					                <span>Are you sure you wish to delete this content?</span>
-					            </div>
-					            <div class="modal-footer">
-					                <form method="post" class="edit_form" action="/admin/json">
-					                <input type="hidden" name="class" value="dash">
-					                <input type="hidden" name="function" value="do_delete">
-					                <input type="hidden" name="type" value="<?php echo $types[$type]['slug']; ?>">
-					                <input type="hidden" name="id" value="<?php echo $_GET['id']; ?>">
-					                <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
-					                <button type="submit" class="btn btn-danger">Yes, delete it</button>
-					                </form>
-					            </div>
-					            </div>
-					        </div>
-					        </div>
+<div class="modal fade" id="delete_conf_<?=$_GET['id']?>" tabindex="-1" role="dialog">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Confirm</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <span>Are you sure you wish to delete this content?</span>
+            </div>
+            <div class="modal-footer">
+                <form method="post" class="edit_form" action="/admin/json">
+                    <input type="hidden" name="class" value="dash">
+                    <input type="hidden" name="function" value="do_delete">
+                    <input type="hidden" name="type" value="<?=$types[$type]['slug']?>">
+                    <input type="hidden" name="id" value="<?=$_GET['id']?>">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                    <button type="submit" class="btn btn-danger">Yes, delete it</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
 
-					        <?php endif;?>
-
-    <?php endif;?>
-
-<?php include_once __DIR__ . '/footer.php';?>
+<?php endif ?>
+<?php endif ?>
+<?php include_once __DIR__.'/footer.php';?>
