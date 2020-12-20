3eaa7171a0ce2ffdb3924c19ad04f5fbf518bd9d
akshay / jigmet <akshay.wildfire@pm.me>
2020-12-21 00:58:58 +0530

    removed google map integration from footer file

--- footer.php
+++ footer.php
@@ -50,9 +50,6 @@
 	<script src="https://blueimp.github.io/jQuery-File-Upload/js/vendor/jquery.ui.widget.js"></script>
 	<script src="https://blueimp.github.io/jQuery-File-Upload/js/jquery.iframe-transport.js"></script>
 	<script src="https://blueimp.github.io/jQuery-File-Upload/js/jquery.fileupload.js"></script>
-	<?php if (defined('GOOGLE_MAP_API_KEY_1') && !empty(GOOGLE_MAP_API_KEY_1)): ?>
-		<script src="https://maps.googleapis.com/maps/api/js?key='<?=GOOGLE_MAP_API_KEY_1?>'&libraries=places&callback=initMap" async defer></script>
-	<?php endif;?>
 
 	<?php if (isset($types['webapp']['admin_confetti'])): ?>
 		<script src="https://cdn.jsdelivr.net/gh/mathusummut/confetti.js/confetti.min.js"></script>
