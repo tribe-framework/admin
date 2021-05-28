1a4e6b95701c05aec30b21bcf685097c706dde6f
akshay / tw <akshay.wildfire@pm.me>
2021-05-28 19:56:09 +0530

    key value pairs of various kinds

--- log.php
+++
@@ -1,3 +0,0 @@
-<?php include_once __DIR__ . '/header.php';
-print_r($dash->do_shell_command('tail -200 /var/log/apache2/error.log'));
-include_once __DIR__ . '/footer.php';
---
+++ meta.php
@@ -0,0 +1,49 @@
+<?php include_once __DIR__ . '/header.php';?>
+
+<?php
+use Wildfire\Api\Api as Api;
+$api = new Api();
+?>
+
+<?php echo $admin->get_admin_menu('dash'); ?>
+
+<div class="container">
+    <a name="infos"></a>
+    <div id="infos" class="d-none alert alert-success"></div>
+
+    <a name="errors"></a>
+    <div id="errors" class="d-none alert alert-danger"></div>
+</div>
+
+<div class="card-group m-0">
+	<div class="card my-2">
+	  <div class="card-header">Key-value pairs</div>
+	  <div class="card-body">
+	    <p class="card-text">On the front-end key-value pairs can be accessed from $dash->get_content($id). Usually used to save website metadata.</p>
+	    <form id="key_value_pair_edit_form" method="post" class="edit_form" action="/admin/json" autocomplete="off">
+    	    <input type="text" class="form-control" name="meta_key" placeholder="Key">
+    	    <input type="text" class="form-control" name="meta_value" placeholder="Value">
+	    	<input type="hidden" name="class" value="dash">
+	    	<input type="hidden" name="type" value="key_value_pair">
+	    	<input type="hidden" name="function" value="push_content">
+	    	<button type="submit" class="btn btn-outline-primary border-top-0 border-left-0 border-right-0 rounded-0 save_btn" data-form-id="key_value_pair_edit_form"><span class="fa fa-save"></span>&nbsp;Save</button>
+	    </form>
+	  </div>
+	</div>
+	<div class="card my-2">
+	  <div class="card-header">API keys</div>
+	  <div class="card-body">
+	    <p class="card-text">The API secret is displayed only once, when the API key is created.</p>
+	    <form id="api_key_secret_edit_form" method="post" class="edit_form" action="/admin/json" autocomplete="off">
+    	    <input type="text" class="form-control" name="api_key" placeholder="API Key" value="<?=uniqid()?>">
+    	    <input type="text" class="form-control" name="api_secret" placeholder="API Secret" value="<?=$api->guidv4()?>">
+	    	<input type="hidden" name="class" value="dash">
+	    	<input type="hidden" name="type" value="api_key_secret">
+	    	<input type="hidden" name="function" value="push_content">
+	    	<button type="submit" class="btn btn-outline-primary border-top-0 border-left-0 border-right-0 rounded-0 save_btn" data-form-id="api_key_secret_edit_form"><span class="fa fa-save"></span>&nbsp;Save</button>
+	    </form>
+	  </div>
+	</div>
+</div>
+
+<?php include_once __DIR__ . '/footer.php';?>
