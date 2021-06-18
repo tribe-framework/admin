5d16c18ca3668a2978fdb8ff171e41b759f7212b
akshay / tw <akshay.wildfire@pm.me>
2021-06-18 14:42:35 +0530

    removed user_pref

--- plugins/typeout/typeout.js
+++ plugins/typeout/typeout.js
@@ -1,7 +1,4 @@
 $( document ).ready(function() {
-	user_pref("capability.policy.policynames", "allowclipboard");
-    user_pref("capability.policy.allowclipboard.Clipboard.cutcopy", "allAccess");
-    user_pref("capability.policy.allowclipboard.Clipboard.paste", "allAccess");
 	
     document.execCommand('enableObjectResizing');
 	document.execCommand('enableInlineTableEditing');
