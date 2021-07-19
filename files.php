7711ebee8a36fb5f69805c4d0e4a5d0cef057f11
akshay / tw <akshay.wildfire@pm.me>
2021-07-19 09:36:59 +0530

    memory issues in file view

--- files.php
+++ files.php
@@ -3,6 +3,7 @@
 /**
  * this code is responsible for listing the content in admin dash
  */
+set_time_limit(600);
 ?>
 
 <div class="p-3">
@@ -56,7 +57,8 @@ $eval_sql_query=implode(' UNION ', $eval_sql);
 $posts=$sql->executeSQL($eval_sql_query);
 
 foreach ($posts as $post) {
-    $xs_file=$dash->get_uploaded_image_in_size($post['file'], 'xs')['url'];
+    //$xs_file=$dash->get_uploaded_image_in_size($post['file'], 'xs')['url'];
+    $xs_file=$post['file'];
     $tr_echo = '<tr><th scope="row">'.$post['id'].'</th><td>'.(exif_imagetype($xs_file)?'<img loading="lazy" src="'.$xs_file.'" width="80">':'<span class="fa-3x fas fa-file-alt"></span>').'</td><td>' . basename($post['file']) . '<br>used in <a href="/'.$post['type'].'/'.$post['slug'].'" target="new">'.$post['title'].'&nbsp;<span class="fas fa-external-link-alt"></span></a></td><td><span class="copy_btn btn btn-sm btn-outline-primary px-3 text-capitalize" data-clipboard-text="[['.$post['file'].']]"><span class="fas fa-copy mr-1"></span>copy shortcode</span></td><td><a href="'.$post['file'].'" target="new"><span class="fas fa-2x fa-external-link-square-alt"></span></a></td></tr>';
     echo $tr_echo;
 }
