318b97cf4324a0fddd2ee50e581827126a7947a2
akshay / tw <akshay.wildfire@pm.me>
2021-11-25 01:41:52 +0530

    frame for Search by ID

--- index.php
+++ index.php
@@ -3,21 +3,28 @@
 <?php echo $admin->get_admin_menu('dash'); ?>
 
 <div class="card-group m-0">
+    <?php if ($currentUser['role']=='admin') { ?>
+    <div class="card my-2">
+        <div class="card-header">Search by ID</div>
+        <div class="card-body">
+            <div class="input-group mb-3">
+              <input type="text" class="form-control" placeholder="Type any ID used in the system">
+              <div class="input-group-append">
+                <button class="btn btn-outline-secondary" type="button">Get data</button>
+              </div>
+            </div>
+            <div class="border border-1 border-dark">
+                <pre><?=json_encode($dash->get_content(1), JSON_PRETTY_PRINT) ?></pre>
+            </div>
+        </div>
+    </div>
+    <?php } ?>
     <div class="card my-2">
         <div class="card-header">Analytics</div>
         <div class="card-body">
             <p class="card-text">Porro quisquam et aliquid quas assumenda sunt culpa. Laudantium eum ex aut accusantium
                 consequuntur dolor sed. Inventore quam quod est ut fugiat cumque beatae. Suscipit eaque non autem
                 dignissimos voluptatibus quo et. Commodi aliquam est aut incidunt voluptatem et.</p>
-        </div>
-    </div>
-    <div class="card my-2">
-        <div class="card-header">Latest</div>
-        <div class="card-body">
-            <p class="card-text">Eos et ut voluptas ad. Vero quis nihil quis impedit omnis ut. Quod non nesciunt illum
-                qui in quidem repellendus libero. Odio molestiae voluptate neque vero architecto esse sunt quae. Quod
-                molestiae est ut tenetur autem esse voluptas itaque. Et similique sunt ipsa libero numquam blanditiis.
-            </p>
         </div>
     </div>
 </div>
