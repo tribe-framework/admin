e2b98ac812fe3fc4f808a3e3c1699fa1b2ef8293
akshay / tw <akshay.wildfire@pm.me>
2021-11-25 00:50:52 +0530

    input_lang spelling

--- list-json.php
+++ list-json.php
@@ -44,7 +44,7 @@ foreach ($ids as $arr) {
     $donotlist = 0;
     foreach ($types[$this_type]['modules'] as $module) {
         if (isset($module['list_field']) && $module['list_field']) {
-            $module_input_slug_lang = $module['input_slug'] . (isset($module['input_land']) && is_array($module['input_lang']) ? "_{$module['input_lang'][0]['slug']}" : '');
+            $module_input_slug_lang = $module['input_slug'] . (isset($module['input_lang']) && is_array($module['input_lang']) ? "_{$module['input_lang'][0]['slug']}" : '');
             $cont = $dash->get_content_meta($post['id'], $module_input_slug_lang);
 
             if (isset($module['list_non_empty_only']) && $module['list_non_empty_only'] && !trim($cont)) {
