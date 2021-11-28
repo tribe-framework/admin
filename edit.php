1b8b645a365300b6f587dcf25f66694da83491ca
Apurv Jyotirmay <mail@jyotirmay.dev>
2021-11-29 04:48:10 +0530

    using ignored_keys list from Dash

--- edit.php
+++ edit.php
@@ -148,7 +148,7 @@ if (!($post_type = $post['type'])) {
 
     <?php
     if ($post) {
-        $ignored_keys = ['type', 'function', 'class', 'slug', 'id', 'updated_on', 'created_on', 'user_id', 'files_descriptor', 'password_md5', 'role_slug', 'mysql_activity_log'];
+        $ignored_keys = $dash->ignored_keys;
 
         foreach ($post as $key => $value) {
             $modules = array_column($types[$type]['modules'], 'input_slug');
