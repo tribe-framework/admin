572a11c55f16201eb5e2da9bd386dbebbb9ab57a
Apurv Jyotirmay <mail@jyotirmay.dev>
2022-07-14 17:22:26 +0530

    fixed errors with select menu in forms

--- theme/includes/form/form-components/select.php
+++ theme/includes/form/form-components/select.php
@@ -1,3 +1,12 @@
+<?php
+/**
+ * @var string $module_input_slug_lang
+ * @var string $module_input_placeholder
+ * @var array $module_input_options
+ * @var array $types
+ * @var object $dash
+ */
+?>
 <div class="form-group mt-5">
     <select class="form-control pl-0 border-top-0 border-left-0 border-right-0 rounded-0 mt-1" id="select_<?= $module_input_slug_lang ?>" name="<?= $module_input_slug_lang ?>">
         <option <?= ($post[$module_input_slug_lang] ?? false)?'':'selected="selected"'; ?> value="">
@@ -11,7 +20,7 @@
                     $_title = $opt['title'] ?? '';
                     $_selected = '';
 
-                    if (isset($post) && isset($opt)) {
+                    if (isset($post)) {
                         $_selected = (($post[$module_input_slug_lang] ?? null) == $opt['slug']) ? 'selected="selected"' : '';
                     }
 
@@ -23,10 +32,13 @@
         } else {
             $options=$dash->get_all_ids($module_input_slug_lang, $types[$module_input_slug_lang]['primary_module'], 'ASC');
             foreach ($options as $opt) {
-                $option=$dash->get_content($opt['id']);
+                $option=$dash->getObject($opt['id']);
                 $titler=$dash->get_type_title_data($option['type']);
                 $title_slug=$titler['slug'];
-                echo '<option value="'.$option['slug'].'" '.(($post[$module_input_slug_lang]==$option['slug'])?'selected="selected"':'').'>'.$option[$title_slug].'</option>';
+
+                $is_selected = isset($post[$module_input_slug_lang]) ? "selected='selected'" : '';
+
+                echo "<option value='{$option['slug']}' {$is_selected}>{$option[$title_slug]}</option>";
             }
         }
         ?>
