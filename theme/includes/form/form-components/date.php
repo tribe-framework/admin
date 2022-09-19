f41a1021afd56ba7a6a5d58708ae648e74ba0eef
Apurv Jyotirmay <mail@jyotirmay.dev>
2022-09-19 18:45:13 +0530

    fixed date and select errors

--- theme/includes/form/form-components/date.php
+++ theme/includes/form/form-components/date.php
@@ -1,7 +1,33 @@
+<?php
+/**
+ * @var array $types
+ * @var array $post
+ * @var string $type
+ * @var string $module_input_slug_lang
+ * @var string $module_input_placeholder
+ * @var string $module_input_default_value
+ */
+
+$_placeholder = $module_input_placeholder ?: ucfirst($types[$type]['name']).' '.$module_input_slug_lang;
+$_value = $post[$module_input_slug_lang] ?? $module_input_default_value;
+?>
 <div class="input-group mt-5">
-<div class="input-group-prepend">
-    <span class="input-group-text border-top-0 border-left-0 border-right-0 rounded-0" id="basic-addon1"><span class="fas fa-calendar"></span></span>
-</div>
-<input type="date" name="<?php echo $module_input_slug_lang; ?>" class="form-control border-top-0 border-left-0 border-right-0 rounded-0 m-0" placeholder="<?php echo($module_input_placeholder?$module_input_placeholder:ucfirst($types[$type]['name']).' '.$module_input_slug_lang); ?>" value="<?php echo($post[$module_input_slug_lang]?$post[$module_input_slug_lang]:$module_input_default_value); ?>">
-<?php echo($module_input_placeholder?'<div class="col-12 row text-muted small m-0"><span class="ml-auto mr-0">'.$module_input_placeholder.'</span></div>':''); ?>
+    <div class="input-group-prepend">
+        <span class="input-group-text border-top-0 border-left-0 border-right-0 rounded-0" id="basic-addon1">
+            <i class="fas fa-calendar"></i>
+        </span>
+    </div>
+
+    <input
+            type="date"
+            name="<?= $module_input_slug_lang ?>"
+            class="form-control border-top-0 border-left-0 border-right-0 rounded-0 m-0"
+            placeholder="<?= $_placeholder ?>"
+            value="<?= $_value ?>">
+
+    <?php
+        if (isset($module_input_placeholder)) {
+            echo "<div class='col-12 row text-muted small m-0'><span class='ml-auto mr-0'>$module_input_placeholder</span></div>";
+        }
+    ?>
 </div>
--- theme/includes/form/form-components/select.php
+++ theme/includes/form/form-components/select.php
@@ -6,39 +6,70 @@
  * @var array $types
  * @var object $dash
  */
+
+$is_selected = '';
+if ($post[$module_input_slug_lang] ?? false) {
+    $is_selected = "selected='selected'";
+}
 ?>
 <div class="form-group mt-5">
-    <select class="form-control pl-0 border-top-0 border-left-0 border-right-0 rounded-0 mt-1" id="select_<?= $module_input_slug_lang ?>" name="<?= $module_input_slug_lang ?>">
-        <option <?= ($post[$module_input_slug_lang] ?? false)?'':'selected="selected"'; ?> value="">
-            <?=$module_input_placeholder?$module_input_placeholder:'Select '.$module_input_slug_lang ?>
+    <select
+            id="select_<?= $module_input_slug_lang ?>"
+            name="<?= $module_input_slug_lang ?>"
+            class="form-control pl-0 border-top-0 border-left-0 border-right-0 rounded-0 mt-1" >
+        <option <?= $is_selected ?> value="">
+            <?= $module_input_placeholder ? $module_input_placeholder : "Select $module_input_slug_lang" ?>
         </option>
+
         <?php
-        if ($options=$module_input_options) {
+        if ($options = $module_input_options) {
             foreach ($options as $opt) {
                 if (is_array($opt)) {
                     $_slug = $opt['slug'] ?? '';
                     $_title = $opt['title'] ?? '';
-                    $_selected = '';
+                    $is_selected = '';
 
-                    if (isset($post) && isset($opt)) {
-                        $_selected = (($post[$module_input_slug_lang] ?? null) == $opt['slug']) ? 'selected="selected"' : '';
+                    if (
+                            isset($post) &&
+                            ($post[$module_input_slug_lang] ?? null) == $opt['slug']
+                    ) {
+                        $is_selected = "selected='selected'";
                     }
 
-                    echo "<option value='$_slug' $_selected>$_title</option>";
-                } else {
-                    echo '<option value="'.$opt.'" '.(($post[$module_input_slug_lang]==$opt)?'selected="selected"':'').'>'.$opt.'</option>';
+                    echo "<option value='$_slug' $is_selected>$_title</option>";
+                }
+                else {
+                    $is_selected = '';
+
+                    if ($post[$module_input_slug_lang] == $opt) {
+                        $is_selected = "selected='selected'";
+                    }
+
+                    echo "<option value='$opt' $is_selected>$opt</option>";
                 }
             }
         } else {
-            $options=$dash->get_all_ids($module_input_slug_lang, $types[$module_input_slug_lang]['primary_module'], 'ASC');
+            $options = $dash->get_all_ids($module_input_slug_lang, $types[$module_input_slug_lang]['primary_module'], 'ASC');
+
             foreach ($options as $opt) {
-                $option=$dash->getObject($opt['id']);
-                $titler=$dash->get_type_title_data($option['type']);
-                $title_slug=$titler['slug'];
-                echo '<option value="'.$option['slug'].'" '.(($post[$module_input_slug_lang]==$option['slug'])?'selected="selected"':'').'>'.$option[$title_slug].'</option>';
+                $option = $dash->getObject($opt['id']);
+                $titler = $dash->get_type_title_data($option['type']);
+                $title_slug = $titler['slug'];
+
+                $is_selected = '';
+                if (($post[$module_input_slug_lang] ?? null) == $option['slug']) {
+                    $is_selected = "selected = 'selected'";
+                }
+
+                echo "<option value='{$option['slug']}' $is_selected>{$option[$title_slug]}</option>";
             }
         }
         ?>
     </select>
-    <?php echo($module_input_placeholder?'<div class="col-12 row text-muted small m-0"><span class="ml-auto mr-0">'.$module_input_placeholder.'</span></div>':''); ?>
+
+    <?php
+    if ($module_input_placeholder) {
+        echo "<div class='col-12 row text-muted small m-0'><span class='ml-auto mr-0'>$module_input_placeholder</span></div>";
+    }
+    ?>
 </div>
